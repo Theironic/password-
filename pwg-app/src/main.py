14bd6b3a7613a3b2249f2b1a0f71d112d8ecbfae
LuccaSouza <theironic117@proton.me>
2025-01-15 10:29:48 -0300

    adicionado novo tipo de tipagem e pequenos comentarios no codigo, Lucca.souza

--- pwg-app/src/main.py
+++ pwg-app/src/main.py
@@ -1,13 +1,17 @@
-
 import flet as ft
-import typing
+from typing import NewType
+# tipagem
+TextField = NewType('TextField', str)
+ElevatedButton = NewType("ElevatedButton", str)
+Text = NewType("Text",str)
 
-qntd_input = ft.TextField("numeros")
-btn_gera = ft.ElevatedButton("gerar senha")
-main_texto= ft.Text("hello world",size=50,color="#003F91",font_family="Courier")
+#variaveis de interface
+qntd_input: TextField= ft.TextField("numeros")
+btn_gera: ElevatedButton = ft.ElevatedButton("gerar senha")
+main_texto: Text = ft.Text("hello world",size=50,color="#003F91",font_family="Courier")
 
 
-
+# main interface
 def main(page: ft.Page):
     def btn_gerar(e)->None:
         
